969b1556b5d417c3aa5e93dce1cd28d4eef1ca08
miguel76 <miguel.ceriani@gmail.com>
2016-06-05 11:33:18 -0500

    Fix bugs

--- index.js
+++ index.js
@@ -17,6 +17,8 @@ var parsedQuery = parser.parse(
     '{ ' +
       '?mickey foaf:name "Mickey Mouse"@en; ' +
           'foaf:knows ?other. ' +
+      'FILTER(STR(?other) < "dfg"). ' +
+      '?mickey foaf:knows/foaf:knows ?donald. ' +
     '}. ' +
     '{ ' +
       '{ ' +
@@ -50,20 +52,30 @@ console.log(prettyjson.render(queryAsSpinPlus));
 // });
 
 var spinPlus2spaAsUpdate = fs.readFileSync('./update/spinPlus2spa.ru', 'utf8');
+// console.log(spinPlus2spaAsUpdate);
+
+// spinPlus2spaAsUpdate = 'INSERT {<http://example.org/s> <http://example.org/p> <http://example.org/o>.} WHERE {?s ?p ?o};';
+// spinPlus2spaAsUpdate = 'INSERT DATA {<http://example.org/s> <http://example.org/p> <http://example.org/o>.};';
 
 async.autoInject({
   store: [rdfstore.create],
   load: function(store, cb) {
     store.load('application/ld+json', queryAsSpinPlus, cb);
   },
+  // parseUpdate: function(store, cb) {
+  //   // var syntaxTree = store.engine.abstractQueryTree.parseQueryString(spinPlus2spaAsUpdate);
+  //   // console.log(syntaxTree);
+  //   cb(null);
+  // },
     // console.log(prettyjson.render(results));
   update: function(store, load, cb) {
+    console.log(load);
     store.execute(spinPlus2spaAsUpdate, cb);
   },
   quads: function(store, update, cb) {
-    console.log(update);
+    // console.log(store);
     rdfstore2quads(store, cb);
   }}, function(err, result) {
     if (err) { console.error(err); }
-    else { console.log(result.quads); }
+    else { console.log(result.quads);  }
   });
